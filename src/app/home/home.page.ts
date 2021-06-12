042e6170ea60c0fe5b9f224b49c6a09dafc1ec33
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-06-12 17:09:11 +0200

    Logout implementiert (aber noch ohne Confirmation-Frage)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,7 @@ import firebase from  'firebase/app';
  * Seite für Firebase-Authentifizierung nach folgender Anleitung:
  * https://github.com/angular/angularfire/blob/master/docs/auth/getting-started.md
  * 
- * Im Firebase-Projekt muss Authentication aktiviert sein! (in Firebase-Projekt auf "Authentication")
+ * Im Firebase-Projekt muss Authentication aktiviert sein! (Firebase-Console: Firebase-Projekt auf "Authentication")
  */
 @Component({
   selector: 'app-home',
@@ -20,9 +20,11 @@ export class HomePage {
    */
   constructor(public firebaseAuth: AngularFireAuth) {}
 
-  
+
   /**
    * Event-Handler für Button "Login".
+   * Nachdem Nutzer eingeloggt ist wird in Firebase-Console unter dem jeweiligen Firebase-Projekt
+   * unter "Authentication | Users" aufgelistet.
    */
   public onLoginButton() {
 
@@ -30,5 +32,13 @@ export class HomePage {
     this.firebaseAuth.signInWithPopup(googleAuthProvider);
     //this.firebaseAuth.signInWithRedirect(googleAuthProvider);
   }
+
+  /**
+   * Event-Handler für Button "Logout".
+   */
+  public onLogoutButton() {
+
+    this.firebaseAuth.signOut();
+  }  
 
 }
